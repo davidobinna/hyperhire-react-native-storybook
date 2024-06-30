74b88ee8331499292d7adf31c2185e9779ea7453
david <davidobinna579@gmail.com>
2024-06-30 08:02:23 +0100

    initial-com

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,59 +1,19 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
-import type { Configuration } from 'webpack';
-
-const config: StorybookConfig = {
+/** @type{import("@storybook/react-webpack5").StorybookConfig} */
+module.exports = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../components/**/*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
-    "@storybook/addon-webpack5-compiler-swc",
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    "@storybook/addon-react-native-web",
   ],
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
   },
-  webpackFinal: async (config: Configuration) => {
-    // Ensure config.module is not undefined
-    config.module = config.module || {};
-
-    // Ensure config.module.rules is not undefined
-    config.module.rules = config.module.rules || [];
-
-    config.module.rules.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      exclude: /node_modules/,
-      use: [
-        {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-            plugins: ['react-native-web'],
-          },
-        },
-      ],
-    });
-
-    config.module.rules.push({
-      test: /\.(avif)$/,
-      use: [
-        {
-          loader: 'file-loader',
-        },
-      ],
-    });
-
-    config.resolve = config.resolve || {};
-    config.resolve.alias = config.resolve.alias || {};
-    config.resolve.alias['react-native$'] = 'react-native-web';
-
-    return config;
+  docs: {
+    autodocs: true,
   },
 };
-
-export default config;
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,12 +1,12 @@
-import 'regenerator-runtime/runtime';
 import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
+        date: /Date$/,
       },
     },
   },
