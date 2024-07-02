709d3196790bdf4d9a15f3dc71a2a73d9cd29d7f
david <davidobinna579@gmail.com>
2024-07-02 10:50:11 +0100

    initial-commit

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Run this command
 - npm install
 - npm run storybook
+
+For Native:
+- npm expo start (wait of 10 - 5mins to bundle);
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,8 +6,6 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/components/useColorScheme';
-
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
@@ -22,7 +20,7 @@ export const unstable_settings = {
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
+{/*   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
@@ -40,20 +38,17 @@ export default function RootLayout() {
 
   if (!loaded) {
     return null;
-  }
+  } 
 
   return <RootLayoutNav />;
+  */}
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="home" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
-    </ThemeProvider>
   );
 }
