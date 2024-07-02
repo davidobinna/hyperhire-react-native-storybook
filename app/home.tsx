b9be48c57d65c48480f0724a00cf92497369b6f1
david <davidobinna579@gmail.com>
2024-07-02 11:45:30 +0100

    initial-commit

--- app/home.tsx
+++ app/home.tsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 import StorybookUIRoot from '../.storybook'; 
-const HomeScreen: React.FC = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to the Home Screen!</Text>
-    </View>
-  );
-};
 
 const App = () => {
-  if (__DEV__) {
-    return <StorybookUIRoot />;
-  }
-
-  return <HomeScreen />;
+    return <StorybookUIRoot />
 };
 
 export default App;
