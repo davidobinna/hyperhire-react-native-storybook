a39995e404d2c10439c6a0c3b4af0a246ec7029e
david <davidobinna579@gmail.com>
2024-07-02 12:07:00 +0100

    initial-commit

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -13,3 +13,22 @@ const preview: Preview = {
 };
 
 export default preview;
+
+
+
+{/* import 'regenerator-runtime/runtime';
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
+ */}
