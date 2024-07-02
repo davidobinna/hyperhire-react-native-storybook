5c49286d10368fab83ba4d9ef24ad66444870aad
david <davidobinna579@gmail.com>
2024-07-02 10:33:20 +0100

    initial-commit

--- stories/Splash.stories.tsx
+++ stories/Splash.stories.tsx
@@ -18,7 +18,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    name: 'John Doe',
+    name: 'Go to Register',
     onSubmit: (data: string) => console.log('Submitted name:', data),
   },
 };
